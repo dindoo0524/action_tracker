1d68d25ab258c59974951e39cf0ef056e9f5a1df
JinjuNa <48050357+JinjuNa@users.noreply.github.com>
2023-07-25 08:35:26 +0900

    fix: fontawesome 문제 해결

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,14 @@
 export default {
+  // mode: 'spa',
   target: 'static',
   router: {
-    base: '/action_tracker/',
+    base: '/action_tracker/'
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'action_tracker',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
@@ -16,19 +17,22 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
       {
         'http-equiv': 'Content-Security-Policy',
-        content: 'upgrade-insecure-requests',
-      },
+        content: 'upgrade-insecure-requests'
+      }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/scss/style.scss'],
+  css: [
+    '@/assets/scss/style.scss',
+    '@fortawesome/fontawesome-svg-core/styles.css'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/vee-validate', ssr: true },
-    { src: '~/plugins/fontawesome', mode: 'client' },
+    { src: '~/plugins/fontawesome' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -39,7 +43,7 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     '@nuxtjs/tailwindcss',
-    '@nuxtjs/dotenv',
+    '@nuxtjs/dotenv'
   ],
 
   devServerHandlers: [],
@@ -50,11 +54,12 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   // build를 설정안하면 ssr이 안되므로 반드시 해야한다.
   build: {
-    transpile: ['vee-validate/dist/rules'],
+    transpile: ['vee-validate/dist/rules']
   },
 
   // server setup
   server: {
-    port: 4000,
-  },
+    // host: '0.0.0.0', // 외부 IP에서 접근 가능하도록 설정
+    port: 4000
+  }
 }
