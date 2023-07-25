19c008a54e7fda9bfd78be72b59df61159b2ed6b
NaJinju <NaJinju@users.noreply.github.com>
2023-07-25 00:34:06 +0000

    deploy: 88097e272de2229219f8bcd50221a7eda29e1405

---
+++ _nuxt/static/1690245245/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Faction_tracker\u002F_nuxt\u002Fstatic\u002F1690245245",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Faction_tracker\u002F",assetsPath:"\u002Faction_tracker\u002F_nuxt\u002F",cdnURL:a}}}}(null));
